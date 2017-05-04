3f3f830aa65a92fb05aa80e463e81141b3ca3765
AH Tunecore <ahoglund@tunecore.com>
2017-05-04 11:50:05 -0400

    add example for config

--- internal_api_example/application.rb
+++ internal_api_example/application.rb
@@ -2,6 +2,10 @@ ExampleApplication.configure do
   internal_api_routes = ["/example"]
   config.middleware.use "InternalApiMiddleware", internal_api_routes
   # OR
+  config.middleware.use "InternalApiMiddleware" do
+    # do some config'ing
+  end
+  # OR
   config.middleware.insert_before 0, "InternalApiMiddleware", internal_api_routes
   # OR
   config.middleware.insert_after 10, "InternalApiMiddleware", internal_api_routes
